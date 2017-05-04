6e75f4ed62f7e683a79aec9d04a530565a4d680a
Rebecca Ansems <ansems_rebecca@hotmail.com>
2017-05-03 21:06:36 -0300

    [A1, Q3] Added submit button and Edit textfield with functionality
    Submit button when clicked takes the text from the edit textfield and
    uses it to replace the Hello World textbox

--- app/src/main/java/com/acme/csci3130/MainActivity.java
+++ app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -2,6 +2,12 @@ package com.acme.csci3130;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +15,17 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.b_Submit);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                EditText mEdit = (EditText)findViewById(R.id.t_Edit);
+                TextView mText = (TextView)findViewById(R.id.t_TextFinal);
+
+                String newText = mEdit.getText().toString();
+
+                mText.setText(newText);
+            }
+        });
     }
 }
